afc366d9f9b6700eb6a640532df769c394a3d637
Leaaaf <alessandro.foglia@outlook.it>
2022-04-30 11:33:24 +0200

    Player apis and openapi file generation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { getFromContainer, MetadataStorage } from 'class-validator';
+import { validationMetadatasToSchemas } from 'class-validator-jsonschema';
+import { writeFileSync } from 'fs';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const OPENAPI_FILE = 'game-backend-openapi.json';
+  const configService = app.get(ConfigService);
   const config = new DocumentBuilder()
     .setTitle('SRS-Game')
     .setDescription('The SRS Project - Group 3')
@@ -12,8 +18,18 @@ async function bootstrap() {
     .addTag('game')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+
+  // Creating all the swagger schemas based on the class validator decorator
+  const metadatas = (getFromContainer(MetadataStorage) as any).validationMetadatas;
+  const schemas = validationMetadatasToSchemas(metadatas);
+
+  if (document && document.components)
+    document.components.schemas = schemas;
+
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT);
+  writeFileSync(OPENAPI_FILE, JSON.stringify(document), {encoding: "utf8"})
+
+  await app.listen(configService.get("PORT"));
 }
 bootstrap();
