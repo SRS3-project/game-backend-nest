352b05e6dafa0057ac3c3116a0fd2d0f5803c343
Leaaaf <alessandro.foglia@outlook.it>
2022-05-06 09:27:18 +0200

    Socket client implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,28 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { FirestoreModule } from './firestore/firestore.module';
-import { GameModule } from './game/game.module';
-import { PlayerModule } from './player/player.module';
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { FirestoreModule } from "./firestore/firestore.module";
+import { GameModule } from "./game/game.module";
+import { PlayerModule } from "./player/player.module";
+import { SocketClientModule } from "./socket/socket-client.module";
 
 @Module({
-  imports: [GameModule, PlayerModule, ConfigModule.forRoot({
-      envFilePath: '.development.env',
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".development.env",
       isGlobal: true,
     }),
     FirestoreModule.forRoot({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
-        keyFilename: configService.get<string>('GOOGLE_APPLICATION_CREDENTIALS'),
+        keyFilename: configService.get<string>("GOOGLE_APPLICATION_CREDENTIALS"),
       }),
       inject: [ConfigService],
     }),
     PlayerModule,
+    GameModule,
+    SocketClientModule,
   ],
   controllers: [AppController],
   providers: [AppService],
