fe71de8b30c43b3a8cb4f4c913a1c60cf6ac4126
Leaaaf <alessandro.foglia@outlook.it>
2022-05-18 17:55:22 +0200

    feat: jwt token validation

--- src/game/game.controller.ts
+++ src/game/game.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Post, Body, Request, Response } from "@nestjs/common";
+import { Controller, Post, Body, Request, Response, UseGuards } from "@nestjs/common";
 import { GameService } from "./game.service";
 import { ApiTags } from "@nestjs/swagger";
 import { CreateAttackDto } from "./dto/create-attack.dto";
 import { CreateArmyDto } from "./dto/create-army.dto";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 
 @ApiTags("game")
 @Controller("game")
@@ -10,11 +11,13 @@ export class GameController {
   constructor(private readonly gameService: GameService) {}
 
   @Post("/attack")
+  @UseGuards(JwtAuthGuard)
   doAttack(@Response() res, @Body() createAttackDto: CreateAttackDto) {
     return this.gameService.createAttack(res, createAttackDto);
   }
 
   @Post("/build")
+  @UseGuards(JwtAuthGuard)
   buildTroop(@Request() req, @Response() res, @Body() createArmyDto: CreateArmyDto) {
     return this.gameService.buildTroop(req, res, createArmyDto);
   }
