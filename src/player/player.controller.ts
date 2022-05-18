45342cd928d273a1b591085dc0cdfe01cf039d6a
Leaaaf <alessandro.foglia@outlook.it>
2022-05-18 17:55:22 +0200

    feat: jwt token validation

--- src/player/player.controller.ts
+++ src/player/player.controller.ts
@@ -1,38 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Res, UsePipes, ValidationPipe } from '@nestjs/common';
-import { PlayerService } from './player.service';
-import { CreatePlayerDto } from './dto/create-player.dto';
-import { UpdatePlayerDto } from './dto/update-player.dto';
-import { ApiTags } from '@nestjs/swagger';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UsePipes,
+  ValidationPipe,
+  UseGuards,
+} from "@nestjs/common";
+import { PlayerService } from "./player.service";
+import { CreatePlayerDto } from "./dto/create-player.dto";
+import { UpdatePlayerDto } from "./dto/update-player.dto";
+import { ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "src/auth/guards/jwt-auth.guard";
 
-@ApiTags('player')
-@Controller('player')
+@ApiTags("player")
+@Controller("player")
 export class PlayerController {
   constructor(private readonly playerService: PlayerService) {}
 
   @Post()
-  @UsePipes(new ValidationPipe({transform: true}))
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(new ValidationPipe({ transform: true }))
   create(@Body() createPlayerDto: CreatePlayerDto) {
     return this.playerService.create(createPlayerDto);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.playerService.findAll();
   }
 
-  @Get(':username')
-  findOne(@Param('username') username: string) {
+  @Get(":username")
+  @UseGuards(JwtAuthGuard)
+  findOne(@Param("username") username: string) {
     return this.playerService.findOne(username);
   }
 
-  @Patch(':username')
-  @UsePipes(new ValidationPipe({transform: true}))
-  update(@Param('username') username: string, @Body() updatePlayerDto: UpdatePlayerDto) {
+  @Patch(":username")
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(new ValidationPipe({ transform: true }))
+  update(@Param("username") username: string, @Body() updatePlayerDto: UpdatePlayerDto) {
     return this.playerService.update(username, updatePlayerDto);
   }
 
-  @Delete(':username')
-  remove(@Param('username') username: string) {
+  @Delete(":username")
+  @UseGuards(JwtAuthGuard)
+  remove(@Param("username") username: string) {
     return this.playerService.remove(username);
   }
 }
