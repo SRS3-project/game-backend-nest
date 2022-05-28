519994a2357c56ef24d68754bc5f80861212df96
Leaaaf <alessandro.foglia@outlook.it>
2022-05-28 12:17:21 +0200

    feat: first refactor and updates

---
+++ src/player/entities/tech.entity.ts
@@ -0,0 +1,7 @@
+import { TechTypeEnum } from "src/game/enum/tech-type.enum";
+import { GenericResource } from "./generic-resource.entity";
+
+export class Tech {
+  type: TechTypeEnum;
+  level: number;
+}
