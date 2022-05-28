39563ddd3f8364458021cfe50f33a34bf96216e2
Leaaaf <alessandro.foglia@outlook.it>
2022-05-28 12:20:35 +0200

    Revert "feat: first refactor and updates"
    This reverts commit 519994a2357c56ef24d68754bc5f80861212df96.

--- src/player/entities/tech.entity.ts
+++
@@ -1,7 +0,0 @@
-import { TechTypeEnum } from "src/game/enum/tech-type.enum";
-import { GenericResource } from "./generic-resource.entity";
-
-export class Tech {
-  type: TechTypeEnum;
-  level: number;
-}
