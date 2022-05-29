0ad01e781402874bfef7cd5af1e6d48efd201a83
Leaaaf <alessandro.foglia@outlook.it>
2022-05-29 14:58:26 +0200

    automated resource generation

---
+++ src/game/resources/resources-production.ts
@@ -0,0 +1,8 @@
+import { ResourceType } from "../enum/resource-type.enum";
+
+export const productionQuantity = [
+  { type: ResourceType.MINERALS, qta: 30 },
+  { type: ResourceType.WOOD, qta: 20 },
+  { type: ResourceType.GOLD, qta: 15 },
+  { type: ResourceType.FOOD, qta: 10 },
+];
